54eac5355b076782075e9dfbfb422154f4094f7c
lukaszbargiel <bigbeerpk@gmail.com>
2019-11-24 22:06:11 +0100

    Adding some interactions with floor map

--- XbimFloorPlanGenerator/XbimFloorPlanGenerator/ClientApp/src/app/floor/floor.component.ts
+++ XbimFloorPlanGenerator/XbimFloorPlanGenerator/ClientApp/src/app/floor/floor.component.ts
@@ -7,18 +7,12 @@ import { ViewPortComponent } from '../viewport/viewport.component';
     templateUrl: './floor.component.html'
 })
 export class FloorComponent implements OnInit  {
-    @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
     @ViewChild('viewport', { static: true }) private viewPort: ViewPortComponent;
     
     public ifcFloor: IfcFloor;
 
     private baseUrl: string;
-    private minX = 99999999;
-    private minY = 99999999;
-    private maxY = 0;
-    private scaleFactor = 1;
-
-    selectedSpace: any;
+    selectedSpace: IfcSpace;
 
     constructor(
         private route: ActivatedRoute,
@@ -26,136 +20,21 @@ export class FloorComponent implements OnInit  {
         this.baseUrl = baseUrl;
     }
 
-    private ctx: CanvasRenderingContext2D;
-
     ngOnInit() {
-        this.ctx = this.canvas.nativeElement.getContext('2d');
         this.route.paramMap.subscribe(params => {
             this.http.get<IfcFloor>(this.baseUrl + 'floor-plan/' + params.get('id')).subscribe(result => {
                 this.ifcFloor = result;
-                this.setTransformFactors();
-                this.drawFloorPlan();
                 this.viewPort.redraw(this.ifcFloor);
                 
             }, error => console.error(error));
         });
-        //this.ctx.canvas.addEventListener('mousemove', this.selectRoomOnMouseOver);
     }
 
-    selectRoomOnMouseOver = (e) => {
-        const mousePos = {
-            x: e.clientX - this.ctx.canvas.offsetLeft,
-            y: e.clientY - this.ctx.canvas.offsetTop
-        };
-        //console.log(mousePos.x + ',' + mousePos.y);
+    selectSpace(spaceId: number) {
+        const selectingSpace = this.ifcFloor.spaces.find(space => space.id === spaceId);
+        this.selectedSpace = selectingSpace;
     }
 
-    selectSpaceOnCanvas() {
-        const data = JSON.parse(this.selectedSpace.serializedShapeGeometry);
-
-        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-        this.drawFloorPlan();
-        this.ctx.beginPath();
-        this.ctx.fillStyle = '#9cf';
-        this.drawProduct(data);
-        this.ctx.closePath();
-        this.ctx.fill();
-
-    }
-
-    setTransformFactors() {
-        let maxX = 0;
-        this.maxY = 0;
-
-        this.ifcFloor.walls.forEach((wall) => {
-            const productGeometryData = JSON.parse(wall.serializedShapeGeometry);
-            productGeometryData.forEach((polygonSet) => {
-                polygonSet.Polygons.forEach((polygon) => {
-
-                    let tmpMinX, tmpMinY = 99999999;
-                    let tmpMaxX, tmpMaxY = 0;
-
-                    const minWallX = polygon.PolygonVertices.reduce(function (prev, curr) {
-                        return prev.X < curr.X ? prev : curr;
-                    });
-                    const minWallY = polygon.PolygonVertices.reduce(function (prev, curr) {
-                        return prev.Y < curr.Y ? prev : curr;
-                    });
-                    const maxWallX = polygon.PolygonVertices.reduce(function (prev, curr) {
-                        return prev.X > curr.X ? prev : curr;
-                    });
-                    const maxWallY = polygon.PolygonVertices.reduce(function (prev, curr) {
-                        return prev.Y > curr.Y ? prev : curr;
-                    });
-                    tmpMinX = minWallX.X;
-                    tmpMinY = minWallY.Y
-                    tmpMaxX = maxWallX.X;
-                    tmpMaxY = maxWallY.Y;
-
-
-
-                    if (this.minX > tmpMinX) {
-                        this.minX = tmpMinX;
-                    }
-                    if (maxX < tmpMaxX) {
-                        maxX = tmpMaxX;
-                    }
-                    if (this.minY > tmpMinY) {
-                        this.minY = tmpMinY;
-                    }
-                    if (this.maxY < tmpMaxY) {
-                        this.maxY = tmpMaxY;
-                    }
-                });
-
-            });
-        });
-        let scaleFactorX = (maxX - this.minX) / 1000;
-        let scaleFactorY = (this.maxY - this.minY) / 1000;
-        this.scaleFactor = Math.max(scaleFactorX, scaleFactorY);
-    }
-    drawFloorPlan() {
-        //this.ctx.fillStyle = 'black';
-
-        this.ifcFloor.walls.forEach((wall) => {
-            //if (wall.id != 2662) {
-            //    return;
-            //}
-            //debugger;
-            const data = JSON.parse(wall.serializedShapeGeometry);
-
-            //this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-            //this.drawFloorPlan();
-            this.ctx.fillStyle = '#666666';
-            this.drawProduct(data);
-            wall.openings.forEach((opening) => {
-                this.ctx.fillStyle = '#e6f2ff';
-                const data = JSON.parse(opening.serializedShapeGeometry);
-                this.drawProduct(data);
-            });
-        });
-
-    }
-
-    drawProduct(productGeometryData: any) {
-        var revertFactor = ((this.maxY - this.minY) / this.scaleFactor);
-        productGeometryData.forEach((polygonSet) => {
-            polygonSet.Polygons.forEach((polygon) => {
-                this.ctx.strokeStyle = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
-                this.ctx.beginPath();
-                // since Y axis starts at the bottom of the canvas which is in opposite to IFC we need to cast Y coordinates based on formulat Ynew = YMax - Y
-                this.ctx.moveTo((polygon.PolygonVertices[0].X - this.minX) / this.scaleFactor, revertFactor - (polygon.PolygonVertices[0].Y - this.minY) / this.scaleFactor);
-                polygon.PolygonVertices.forEach((vertice) => {
-                    this.ctx.lineTo((vertice.X - this.minX) / this.scaleFactor, revertFactor - (vertice.Y - this.minY) / this.scaleFactor);
-                });
-                
-                this.ctx.closePath();
-                this.ctx.fill();
-
-                //this.ctx.fill();
-            });
-        });
-    }
 }
 
 export interface IfcFloor {
@@ -182,7 +61,7 @@ interface IfcOpening {
     serializedShapeGeometry: string;
 }
 
-interface IfcSpace {
+export interface IfcSpace {
     id: number;
     entityLabel: string;
     description: string;
--- XbimFloorPlanGenerator/XbimFloorPlanGenerator/ClientApp/src/app/viewport/viewport.component.ts
+++ XbimFloorPlanGenerator/XbimFloorPlanGenerator/ClientApp/src/app/viewport/viewport.component.ts
@@ -1,13 +1,13 @@
-import { OnInit, Component, ElementRef, NgZone, Input } from '@angular/core';
-import { Application, Sprite, Text, Polygon, Point, Graphics } from 'pixi.js';
+import { OnInit, Component, ElementRef, NgZone, Input, OnChanges, SimpleChanges, SimpleChange, Output, EventEmitter } from '@angular/core';
+import { Application, Sprite, Text, Polygon, Point, Graphics, filters, TextStyle } from 'pixi.js';
 import { Viewport } from 'pixi-viewport'
-import { IfcFloor } from '../floor/floor.component';
+import { IfcFloor, IfcSpace } from '../floor/floor.component';
 @Component({
     selector: 'app-view-port',
     template: ''
 })
-export class ViewPortComponent implements OnInit {
-    
+export class ViewPortComponent implements OnInit, OnChanges  {
+    @Input('selected-space') selectedSpace: IfcSpace;
     public app: Application;
     public viewport: Viewport;
     private minX = 99999999;
@@ -15,9 +15,30 @@ export class ViewPortComponent implements OnInit {
     private maxY = 0;
     private scaleFactor = 1;
     private revertFactor = 1;
-    
-    constructor(private elementRef: ElementRef, private ngZone: NgZone) { }
+    public spaceGraphics: { [id: number]: Graphics; } = {};
+    public spaceLabelStyle = new TextStyle({
+        fontSize: 36
+    });
 
+    constructor(private elementRef: ElementRef, private ngZone: NgZone) { }
+    @Output() selectSpaceEvent = new EventEmitter<number>();
+
+    ngOnChanges(changes: SimpleChanges) {
+        const currentItem: SimpleChange = changes.selectedSpace;
+        if (currentItem.previousValue) {
+            const unselectedSpaceGraphic = this.spaceGraphics[currentItem.previousValue.id];
+            const colorMatrix = this.getColorMatrix(0xffffff);
+            unselectedSpaceGraphic.filters = [colorMatrix];        
+        }
+        if (currentItem.currentValue) {
+            const selectedSpaceGraphic = this.spaceGraphics[currentItem.currentValue.id];                    
+            const colorMatrix = this.getColorMatrix(0x94ffa8);
+            selectedSpaceGraphic.filters = [colorMatrix];        
+        }
+
+    }
+    
+    // This method let us to calculate factors that are later use to scale the given points to match canvas    
     setTransformFactors(floorDefinition: IfcFloor) {
         let maxX = 0;
         this.maxY = 0;
@@ -68,62 +89,105 @@ export class ViewPortComponent implements OnInit {
         let scaleFactorX = (maxX - this.minX) / 1000;
         let scaleFactorY = (this.maxY - this.minY) / 1000;
         this.scaleFactor = Math.max(scaleFactorX, scaleFactorY);
-        let revertFactor = ((this.maxY - this.minY) / this.scaleFactor);
+        this.revertFactor = ((this.maxY - this.minY) / this.scaleFactor);
     }
+
     redraw(floorDefinition: IfcFloor): void {
-        
+
         if (!floorDefinition) {
             return;
         }
 
         this.setTransformFactors(floorDefinition);
+
         floorDefinition.walls.forEach((wall) => {
-
             const productGeometryData = JSON.parse(wall.serializedShapeGeometry);
-            this.drawFloorShape(productGeometryData, 0x666666);
+            const wallGraphic = this.generateGraphics(productGeometryData, 0x666666, wall.id);
+            this.viewport.addChild(wallGraphic);
 
-            wall.openings.forEach((opening) => {                
+            wall.openings.forEach((opening) => {
                 const data = JSON.parse(opening.serializedShapeGeometry);
-                this.drawFloorShape(data, 0xe6f2ff);
+                const openingGraphic = this.generateGraphics(data, 0xe6f2ff);
+                this.viewport.addChild(openingGraphic);
             });
         });
+
+        floorDefinition.spaces.forEach((space) => {
+            const productGeometryData = JSON.parse(space.serializedShapeGeometry);
+            const spaceGraphic = this.generateGraphics(productGeometryData, 0xffffff, space.id);
+            const bounds = spaceGraphic.getBounds();
+
+            // we cannot simply add text - it needs to be adjust to container size
+            const spaceName = new Text(space.longName, this.spaceLabelStyle);
+            while (spaceName.width > bounds.width) spaceName.style.fontSize--;
+            spaceName.x = bounds.x + (bounds.width - spaceName.width) /2;
+            spaceName.y = bounds.y + bounds.height / 2;
+            spaceGraphic.addChild(spaceName);
+
+            spaceGraphic.on('pointerover', (e) => {
+                const colorMatrix = this.getColorMatrix(0x94ffa8);
+                e.currentTarget.filters = [colorMatrix];
+            });
+            spaceGraphic.on('pointerout', (e) => {
+                const assignedSpaceId = Object.keys(this.spaceGraphics).find(key => this.spaceGraphics[key] === e.currentTarget);
+                if (this.selectedSpace && this.selectedSpace.id === Number(assignedSpaceId)) {
+                    return;
+                }
+                e.currentTarget.filters = null;
+            });
+
+            spaceGraphic.on('pointerdown', (e) => {
+                const selectedSpaceId : number = Number(Object.keys(this.spaceGraphics).find(key => this.spaceGraphics[key] === e.currentTarget));
+                this.selectSpaceEvent.next(selectedSpaceId);
+                
+            });
+            this.viewport.addChild(spaceGraphic);
+
+            this.spaceGraphics[space.id] = spaceGraphic;
+        });
+
     }
 
-    drawFloorShape(productGeometryData: any, color?: number) {
+    generateGraphics(productGeometryData: any, color?: number, shapeId?: number): Graphics {
         let polygonPoints: Point[] = [];
-        
+
+        const polygonGraphics = new Graphics();
+        polygonGraphics.beginFill(color);
+
         productGeometryData.forEach((polygonSet) => {
+
             polygonSet.Polygons.forEach((polygon) => {
                 polygonPoints = [];
                 polygon.PolygonVertices.forEach((vertice) => {
                     polygonPoints.push(new Point((vertice.X - this.minX) / this.scaleFactor, this.revertFactor - (vertice.Y - this.minY) / this.scaleFactor));
                 });
 
-                const ifcPolygon = new Polygon(polygonPoints);                
-                const graphics = new Graphics();
-                graphics.beginFill(color);
-                graphics.drawPolygon(ifcPolygon);
-
-                const text = new Text("WORD");
-                
-                graphics.addChild(text);
-                graphics.interactive = true;                
-                graphics.buttonMode = true;
-                graphics.on('pointerdown', function (e) {
-                    // select
-                });
-                graphics.on('pointerover', function (e) {
-                    // redraw
-                });
-                
-                graphics.endFill();
-
-                this.viewport.addChild(graphics);
+                const ifcPolygon = new Polygon(polygonPoints);
+                polygonGraphics.drawPolygon(ifcPolygon);
+                polygonGraphics.interactive = true;
+                polygonGraphics.buttonMode = true;
 
             });
+
         });
+
+        polygonGraphics.endFill();
+
+        return polygonGraphics;
+
     }
 
+    getColorMatrix(tintColor: any): filters.ColorMatrixFilter {
+        const color = new filters.ColorMatrixFilter();
+        const tint = tintColor;
+        const r = tint >> 16 & 0xFF;
+        const g = tint >> 8 & 0xFF;
+        const b = tint & 0xFF;
+        color.matrix[0] = r / 255;
+        color.matrix[6] = g / 255;
+        color.matrix[12] = b / 255;
+        return color;
+    }
     ngOnInit(): void {
         this.ngZone.runOutsideAngular(() => {
             this.app = new Application({ backgroundColor: 0xffffff });
@@ -137,7 +201,7 @@ export class ViewPortComponent implements OnInit {
 
 
         this.viewport = new Viewport({
-            screenWidth: window.innerWidth,
+            screenWidth: parent.innerWidth,
             screenHeight: window.innerHeight,
             worldWidth: 1000,
             worldHeight: 1000,
@@ -151,10 +215,10 @@ export class ViewPortComponent implements OnInit {
             .drag()
             .pinch()
             .wheel()
-            .decelerate()        
+            .decelerate()
 
 
     }
 
-    
+
 }
